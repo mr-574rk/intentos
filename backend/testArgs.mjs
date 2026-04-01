29406a613747414ac6bd5751f16b1180cd68dbc0
[$] Mr 574rk [$] <66188165+mr-574rk@users.noreply.github.com>
2026-04-01 04:56:18 +0100

    chore: untrack backend test/scratch files

--- backend/testArgs.mjs
+++
@@ -1,6 +0,0 @@
-import { bcs } from "@initia/initia.js";
-
-const strategyId = "421367cd-30ad-4ce2-ab74-28ec5cb103bb";
-const serialized = bcs.string().serialize(strategyId).toBase64();
-console.log("TS bcs.string():", serialized);
-
--- backend/testPK.ts
+++
@@ -1,4 +0,0 @@
-import { RawKey } from "@initia/initia.js";
-const pk = "a53bd229ff460488159ed006d1a4ed2fc63d453bc55f2bc11a1fbf185443894b";
-const key = new RawKey(Buffer.from(pk, "hex"));
-console.log("Relayer address:", key.accAddress);
